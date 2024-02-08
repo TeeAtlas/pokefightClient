20f20e539eb5f54101491cc20d5b024fa28d1758
Tania Boterman <taniaboterman@gmail.com>
2024-02-08 18:29:29 +0100

    basic react routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-// import { useState, useEffect } from 'react';
-// import './App.css'
-// import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-// functon App() {
-//   const [pokemon, setPokemon] = useState([]);
-//   const [pokemons, setPokemons] = useState([]);
-
-//   //set useEffect hook in order to invoke function only when app is loaded 
-//   useEffect(() => {
-
-//     getPokemons()
-
-//   }, []);
+function App() {
+  return <h1>Pokemon fight</h1>
 
 
+  // const [pokemon, setPokemon] = useState([]);
+  // const [pokemons, setPokemons] = useState([]);
+  
+  // //set useEffect hook in order to invoke function only when app is loaded 
+  // useEffect(() => {
+    
+    //   getPokemons()
+    
+    // }, []);
+    
+    
 
-// }
+}
 
 
 
-// export default App;
+export default App;
